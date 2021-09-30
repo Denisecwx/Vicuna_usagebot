a471ddb32bb118bb0dc2b26bf3eacc49d183f7a5
Ubuntu work <denisecwx@gmail.com>
2021-09-30 13:40:01 +0800

    Shorten section titles

--- models.py
+++ models.py
@@ -51,7 +51,7 @@ def get_daily_usage():
         for d in downld_data.all():
             email = d[1]
             if email not in user_stats:
-                user_stats[email] = [None, 0,0,0, None] # [user name, downloads today, downloads for month, downloads for 3 months, active account]
+                user_stats[email] = ["--N/A--", 0,0,0, None] # [user name, downloads today, downloads for month, downloads for 3 months, active account]
             
             data_date = d[date_col]
             data_month = d[date_col].strptime(str(d[date_col]), "%Y-%m-%d %H:%M:%S").month
@@ -63,6 +63,7 @@ def get_daily_usage():
 
             if data_date>=last_five and user_stats[email][4]==None:
                 user_stats[email][4] = True
+                active_count+=1
             elif data_date < last_five and user_stats[email][4]==None:
                 user_stats[email][4] = False
             # number of q downloaded past 3 months
@@ -81,7 +82,7 @@ def get_daily_usage():
                 user_stats[email][0] = users[2]
 
         # create message
-        summary = f"Summary (Define active ac as download docs within the last 5 DAYS):\n---------------\nActive Accounts: {len([1 for x in user_stats if user_stats[x][4]])}/{total_users}\nInactive Accounts:{len([1 for x in user_stats if user_stats[x][4]==False])}/{total_users}"
+        summary = f"Summary (Downloads within the last 5 DAYS):\n---------------\nActive Accounts: {active_count}/{total_users}\nInactive Accounts:{total_users-active_count}/{total_users}"
         active_template = "{1}    ({0}):\n    today : {2}\n    this month : {3}\n    past 3 months : {4}"
         inactive_template = "{1}    ({0}):\n    this month : {3}\n    past 3 months : {4}"
         active_list = []
@@ -100,16 +101,16 @@ def get_daily_usage():
             inactive_str = "--N/A--"
         message = f'{summary}\n\nActive Accounts (Questions downloaded)\n-------------\n{active_str}\n\nInactive Accounts (Questions downloaded)\n-------------\n{inactive_str}'
         
-        # print(message)
+        print(message)
         # send message
-        bot.sendMessage(chat_id=config.TELE_CHAT_ID, text=message)
+        # bot.sendMessage(chat_id=config.TELE_CHAT_ID, text=message)
     
 if __name__ == "__main__":
     get_daily_usage()
     # start job at 8pm everyday
-    scheduler.add_job(get_daily_usage, 'interval', hours=24, start_date=datetime.today().replace(hour=20, minute=0, second=0, microsecond=0))
+    # scheduler.add_job(get_daily_usage, 'interval', hours=24, start_date=datetime.today().replace(hour=20, minute=0, second=0, microsecond=0))
 
-    try:
-        scheduler.start()
-    except (KeyboardInterrupt, SystemExit):
-        pass
+    # try:
+    #     scheduler.start()
+    # except (KeyboardInterrupt, SystemExit):
+    #     pass
