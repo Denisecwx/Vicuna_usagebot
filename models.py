448bb02bf48e13342bad7c86d4c3d7da29534b27
Ubuntu work <denisecwx@gmail.com>
2021-09-30 13:55:38 +0800

    Add condition to excl. data from staff accounts

--- models.py
+++ models.py
@@ -11,6 +11,11 @@ import config
 bot = telegram.Bot(config.PINGUSE_TOKEN)
 scheduler = BlockingScheduler()
 
+acc_to_omit = ["Test2@test.com",
+            "tengxinhui73@gmail.com",
+            "testtest@mail.com",
+            "saraikmalia.vicuna@gmail.com"]
+
 def get_daily_usage():
     '''
     Notify usage stats on daily basis
@@ -30,7 +35,7 @@ def get_daily_usage():
         Check downloads
         '''
         total_users = conn.execute(text("SELECT COUNT(*) FROM users;"))
-        total_users = total_users.first()[0]
+        total_users = total_users.first()[0] - len(acc_to_omit)
         user_stats = {}
 
         # different time ranges
@@ -50,30 +55,25 @@ def get_daily_usage():
         email_col, date_col, nqn_col = 1, 8, 7
         for d in downld_data.all():
             email = d[1]
-            if email not in user_stats:
-                user_stats[email] = ["--N/A--", 0,0,0, None] # [user name, downloads today, downloads for month, downloads for 3 months, active account]
-            
-            data_date = d[date_col]
-            data_month = d[date_col].strptime(str(d[date_col]), "%Y-%m-%d %H:%M:%S").month
+            if email not in acc_to_omit: # check if account belongs to staff
+                if email not in user_stats:
+                    user_stats[email] = ["--N/A--", 0,0,0, None] # [user name, downloads today, downloads for month, downloads for 3 months, active account]
+                
+                data_date = d[date_col]
+                data_month = d[date_col].strptime(str(d[date_col]), "%Y-%m-%d %H:%M:%S").month
 
-            if data_date == today: # number of q downloaded today
-                user_stats[email][1] += parse_number(d[nqn_col])
-            if data_month == this_month: # number of q downloaded this month
+                if data_date == today: # number of q downloaded today
+                    user_stats[email][1] += parse_number(d[nqn_col])
+                if data_month == this_month: # number of q downloaded this month
+                    user_stats[email][2] += parse_number(d[nqn_col])
+
+                if data_date>=last_five and user_stats[email][4]==None:
+                    user_stats[email][4] = True
+                    active_count+=1
+                elif data_date < last_five and user_stats[email][4]==None:
+                    user_stats[email][4] = False
+                # number of q downloaded past 3 months
                 user_stats[email][2] += parse_number(d[nqn_col])
-
-            if data_date>=last_five and user_stats[email][4]==None:
-                user_stats[email][4] = True
-                active_count+=1
-            elif data_date < last_five and user_stats[email][4]==None:
-                user_stats[email][4] = False
-            # number of q downloaded past 3 months
-            user_stats[email][2] += parse_number(d[nqn_col])
-            # try:    # integer
-            #     user_stats[email][2] += d[nqn_col]
-            # except Exception:   # handle old data format
-            #     qnstr = re.sub("[\['\]]", "", d[nqn_col])
-            #     qnstr = sum([int(x) for x in qnstr.split(", ")])
-            #     user_stats[email][3] += qnstr
         
         # get username
         all_users = conn.execute(text("SELECT * FROM users;"))
